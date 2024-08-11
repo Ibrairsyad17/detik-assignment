d925be1c2f8598528cd16d1cd1573b501b9733eb
Ibrahim Irsad <ibrairsyad1712@gmail.com>
2024-08-11 09:34:40 +0700

    refactor: Update asset routing in Vercel configuration

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://unpkg.com/@webpixels/css@1.2.6/dist/index.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('/assets/css/app.css') }}">
+    <link rel="stylesheet" href="/assets/css/app.css">
     {{-- @vite('resources/css/app.css') --}}
 </head>
 
